549aa3ad62b84986b26af33d74bc5f7b8b2c9823
Denilson <denilsonsousa7@outlook.com>
2020-04-18 13:24:58 -0300

    pagina de login criada

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { FiLogIn } from 'react-icons/fi' // feather icons
 import { FiLock } from 'react-icons/fi' // feather icons
 import './styles.css';
 import dogImg from '../../assets/image 2.png';
 import logo from '../../assets/logo.png';
 export default function Login() {
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
+    function handleRegister(e){
+        e.preventDefault();
+
+        console.log(email+" = "+password);
+
+    }
     return (
         <div className="login-container">
             <section className="form">
                
-                <form action="">
+                <form onSubmit={handleRegister}>
                     <img className="logoImg" src={logo} alt="Logo Pet Guardian" />
                     <div className="inputUser">
                         <FiLogIn className="icon" size={20} color="#01D2A7" />
-                        <input placeholder=" exemploemail@gmail.com" type="text" />
+                        <input placeholder=" exemploemail@gmail.com" type="text" value={email} onChange={e => setEmail(e.target.value)}/>
 
                     </div>
                     <div className="inputUser">
                         <FiLock className="icon" size={20} color="#01D2A7" />
-                        <input placeholder=" *******" type="password" />
+                        <input placeholder=" *******" type="password" value= {password} onChange={e => setPassword(e.target.value)}/>
 
                     </div>
                     <div className="links">
@@ -34,8 +43,6 @@ export default function Login() {
                 </form>
 
             </section>
-
-            <img className="dogImg" src={dogImg} alt="Dog" />
         </div>
 
     )
